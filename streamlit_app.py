33ccc57bbb153f25a93896d412d6dddd8a4aa17f
demo2024350 <152519319+demo2024350@users.noreply.github.com>
2023-12-01 14:52:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
-demo
+def adding(x , y):
+    return x + y
+ 
+a = int(input("Enter first number :" ))
+b = int(input("Enter second number :"))
+ 
+sum = adding(a , b)
+ 
+print("addition of {} and {} is {}".format(a,b,sum))
